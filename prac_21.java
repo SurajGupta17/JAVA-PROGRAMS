117e690136e49bb01aad12404fa6ce5f9eff74ac
SURAJ GUPTA <128698813+SJSTUNNER@users.noreply.github.com>
2023-06-14 08:59:43 +0530

    Update prac_21.java

--- prac_21.java
+++ prac_21.java
@@ -1,3 +1,6 @@
+/*Write an application that declares a class named Person. It should have instance variables to record name, age & salary. Use the new operator to create a Person object. Set & display its instance variables.*/
+
+
 import java.util.Scanner;
 class employee{
 
