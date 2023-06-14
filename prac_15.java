66e0af14574e1d36d30f0adc2dd8e292d8a462ee
SURAJ GUPTA <128698813+SJSTUNNER@users.noreply.github.com>
2023-06-14 08:54:24 +0530

    Update prac_15.java

--- prac_15.java
+++ prac_15.java
@@ -1,3 +1,6 @@
+/*Write a method with following method header.public static int gcd(int num1, int num2)Write a program that prompts the user to enter two integers and compute the gcd of two integers*/
+
+
 import java.util.Scanner;
 public class prac_15 {
     public static int gcd(int num1,int num2){
