c61814dd7ff7ee6dcd1c92ba3a1c9b24a449eac3
SURAJ GUPTA <128698813+SJSTUNNER@users.noreply.github.com>
2023-06-14 08:52:53 +0530

    Update prac_14.java

--- prac_14.java
+++ prac_14.java
@@ -1,3 +1,7 @@
+/*Write a program that reads an integer and displays all its smallest factors in increasing order. For example if input number is 120, the output should 
+be as follows:2,2,2,3,5.*/
+
+
 import java.util.Scanner;
 public class prac_14 {
     public static void main(String[] args) {
