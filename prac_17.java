d48032add58e61a2325de08ad53826a74e6a8cfa
SURAJ GUPTA <128698813+SJSTUNNER@users.noreply.github.com>
2023-06-14 08:56:16 +0530

    Update prac_17.java

--- prac_17.java
+++ prac_17.java
@@ -1,8 +1,6 @@
 /*Write a program that generate 6*6 two
 -dimensional matrix, filled with 0’s and 1’s ,display the matrix, check every raw and column have an odd number’s of 1’s.*/
 
-
-
 import java.util.Scanner;
 import java.util.Random;
 
