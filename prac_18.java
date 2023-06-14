98d53c3c0198268a20944418826e394862c8b1b9
SURAJ GUPTA <128698813+SJSTUNNER@users.noreply.github.com>
2023-06-14 08:57:20 +0530

    Update prac_18.java

--- prac_18.java
+++ prac_18.java
@@ -1,3 +1,6 @@
+/*Write a program to sort the elements from given array of integer. Specify size of array and elements of array from command line argument.*/
+
+
 import java.util.Arrays;
 import java.util.Scanner;
 
