401baf845447b3f7e4d227d31e20cdd06f0afe61
SURAJ GUPTA <128698813+SJSTUNNER@users.noreply.github.com>
2023-06-14 09:00:46 +0530

    Update prac_22.java

--- prac_22.java
+++ prac_22.java
@@ -1,3 +1,6 @@
+/*Write a program of constructor overloading and make zero argument constructor to set a default values of student name, roll no and total marks.And make another constructor with all the three parameters and make use of ‘this’ keyword. Using methods, display values of both the constructors*/
+
+
 class detailsOfStudent{
     int marks,rollno;
     String name;
