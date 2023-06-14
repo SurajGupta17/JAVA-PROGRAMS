a65134cbfb541cce717224052b97564f2c298d58
SURAJ GUPTA <128698813+SJSTUNNER@users.noreply.github.com>
2023-06-14 09:01:41 +0530

    Update prac_23.java

--- prac_23.java
+++ prac_23.java
@@ -1,3 +1,6 @@
+/*Write a program that defines a Circle class with two constructors. The first form accepts a double value that represents the radius of the circle. The second form accepts the integer radius of the circle and calculates the area of the circle.*/
+
+
 import java.util.Scanner;
 import java.lang.Math;
 class circle{
